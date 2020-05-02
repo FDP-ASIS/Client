043ee0d5c9a56b9a493a93dfe61835769a1cacc7
Liad Khamdadash <liadkh95@gmail.com>
2020-05-02 16:56:43 +0300

    config electron to work with typescript

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,11 +1,8 @@
-const electron = require("electron");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import isDev from "electron-is-dev";
 
-const path = require("path");
-const isDev = require("electron-is-dev");
-
-let mainWindow;
+let mainWindow: BrowserWindow | null = null;
 
 if (isDev) {
 	const debug = require("electron-debug");
@@ -38,10 +35,10 @@ function createWindow() {
 
 		app.whenReady().then(() => {
 			installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS])
-				.then((name) => {
+				.then((name: string) => {
 					console.log(`Added Extension:  ${name}`);
 				})
-				.catch((err) => console.log("An error occurred: ", err));
+				.catch((err: string) => console.log("An error occurred: ", err));
 		});
 
 		// Open the DevTools.
