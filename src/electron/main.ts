b269d8449ab9c78bd83f8ef9d60acd48d68f26c1
Liad Khamdadash <liadkh95@gmail.com>
2020-05-13 16:21:40 +0300

    change electron to class

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -5,66 +5,80 @@ import * as INSTALLER_EXTENSTION from 'electron-devtools-installer';
 
 declare function require(moduleName: string): any;
 
-let mainWindow: BrowserWindow | null = null;
-
 if (isDev) {
 	const debug = require('electron-debug');
 	debug({ devToolsMode: 'detach' });
 	require('electron-reload')(__dirname);
 }
+class Main {
+	static readonly WIDTH = 900;
+	static readonly HEIGHT = 680;
 
-function createWindow() {
-	let width = 900,
-		height = 680;
-	mainWindow = new BrowserWindow({
-		minWidth: width,
-		minHeight: height,
-		maxWidth: width,
-		maxHeight: height,
-		webPreferences: {
-			nodeIntegration: true,
-		},
-	});
-	mainWindow.setMenuBarVisibility(false);
-	mainWindow.loadURL(
-		isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../index.html')}`
-	);
+	private mainWindow: BrowserWindow | null = null;
 
-	if (isDev) {
-		let installExtension: typeof INSTALLER_EXTENSTION = require('electron-devtools-installer');
-
-		app.whenReady().then(() => {
-			installExtension
-				.default([installExtension.REACT_DEVELOPER_TOOLS, installExtension.REDUX_DEVTOOLS])
-				.then((name) => console.log(`Added Extension:  ${name}`))
-				.catch((err) => console.log('An error occurred: ', err));
-		});
-
-		// Open the DevTools.
-		// BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
-		// mainWindow.webContents.openDevTools();
-	} else {
-		const log = require('electron-log');
-		Object.assign(console, log.functions);
-		// console.log = log.log;
+	public init() {
+		app.on('ready', this.createWindow);
+		app.on('window-all-closed', this.onWindowAllClosed);
+		app.on('activate', this.onActivate);
 	}
-	mainWindow.on('closed', () => (mainWindow = null));
+
+	private onWindowAllClosed() {
+		if (process.platform !== 'darwin') {
+			app.quit();
+		}
+	}
+
+	private onActivate() {
+		//add electron dev tool
+		if (isDev) console.log(require('devtron').install());
+
+		//make the mainWindow as a singleton
+		if (!this.mainWindow && BrowserWindow.getAllWindows().length === 0) {
+			this.createWindow();
+		}
+	}
+
+	private createWindow() {
+		this.mainWindow = new BrowserWindow({
+			minWidth: Main.WIDTH,
+			minHeight: Main.HEIGHT,
+			maxWidth: Main.WIDTH,
+			maxHeight: Main.HEIGHT,
+			height: 600,
+			width: 800,
+			title: `Yet another Electron Application`,
+			webPreferences: {
+				nodeIntegration: true,
+			},
+		});
+		this.mainWindow.setMenuBarVisibility(false);
+		this.mainWindow.loadURL(
+			isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../index.html')}`
+		);
+
+		if (isDev) {
+			let installExtension: typeof INSTALLER_EXTENSTION = require('electron-devtools-installer');
+
+			app.whenReady().then(() => {
+				installExtension
+					.default([
+						installExtension.REACT_DEVELOPER_TOOLS,
+						installExtension.REDUX_DEVTOOLS,
+					])
+					.then((name) => console.log(`Added Extension:  ${name}`))
+					.catch((err) => console.log('An error occurred: ', err));
+			});
+
+			// Open the DevTools.
+			// BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+			// this.mainWindow.webContents.openDevTools();
+		} else {
+			const log = require('electron-log');
+			Object.assign(console, log.functions);
+			// console.log = log.log;
+		}
+		this.mainWindow.on('closed', () => (this.mainWindow = null));
+	}
 }
 
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
-});
-
-app.on('activate', () => {
-	//add electron dev tool
-	if (isDev) console.log(require('devtron').install());
-
-	//make the mainWindow as a singleton
-	if (mainWindow === null && BrowserWindow.getAllWindows().length === 0) {
-		createWindow();
-	}
-});
+new Main().init();
