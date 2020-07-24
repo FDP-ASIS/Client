7d196a4c75fa5a0c0bdae5e20d720947711e3c0a
Slava Kagan <slava15.92@gmail.com>
2020-07-24 18:22:12 +0300

    fix errors

--- src/electron/tools/software.ts
+++ src/electron/tools/software.ts
@@ -12,17 +12,17 @@ export const downloadAndRun = async (url: string, id: string) => {
 
 	const script = path.join(process.cwd(), SCRIPT_FOLDER, id, SCRIPT_NAME);
 
-	// let child = spawn('powershell.exe', ['-executionpolicy', 'bypass', '-file ', script]);
-	// child.stdout.on('data', function (data) {
-	// 	console.log('Powershell Data: ' + data);
-	// });
-	// child.stderr.on('data', function (data) {
-	// 	console.log('Powershell Errors: ' + data);
-	// });
-	// child.on('exit', function () {
-	// 	console.log('Powershell Script finished');
-	// });
-	// child.stdin.end();
+	let child = spawn('powershell.exe', ['-executionpolicy', 'bypass', '-file ', script]);
+	child.stdout.on('data', function (data) {
+		console.log('Powershell Data: ' + data);
+	});
+	child.stderr.on('data', function (data) {
+		console.log('Powershell Errors: ' + data);
+	});
+	child.on('exit', function () {
+		console.log('Powershell Script finished');
+	});
+	child.stdin.end();
 };
 
 export const save = (id: string) => {
