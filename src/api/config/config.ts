b5fdc183444801337bfc142569a5e7c49627c64b
Liad Khamdadash <liadkh95@gmail.com>
2020-07-25 13:24:25 +0300

    fix prod send request

--- src/api/config/config.ts
+++ src/api/config/config.ts
@@ -1,11 +1,12 @@
 import * as qs from 'qs';
 import { PathLike } from 'fs';
+import { proxy } from '../../../package.json';
 
 export const apiConfig = {
 	returnRejectedPromiseOnError: true,
 	withCredentials: true,
 	timeout: 30000,
-	baseURL: '/',
+	baseURL: proxy,
 	headers: {
 		common: {
 			'Cache-Control': 'no-cache, no-store, must-revalidate',
